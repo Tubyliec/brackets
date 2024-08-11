a82b9fc243cfaf418cf699d8b29057c463cc2899
tubyliec <antikruk@vivaldi.net>
2024-08-11 11:54:07 +0300

    feat: add task code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketString = str;
+  
+  let bracketsArray = [];
+  
+  for (let number in bracketsConfig) {
+    bracketsArray[number] = bracketsConfig[number][0] + bracketsConfig[number][1]; // pairing brackets from config
+  }
+
+  for (let i = 0; i <= str.length; i++) {
+      for (let bracketNumber in bracketsConfig) {
+        bracketString = bracketString.split(`${bracketsArray[bracketNumber]}`).join('');  // splitting a string by values ​​from an array
+      }
+  }
+
+  if (bracketString.length == 0) {
+      return (true);
+  } else if (bracketString.length != 0) {
+      return (false);
+  }
 }
